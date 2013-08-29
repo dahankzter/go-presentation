2b182fe4200b6310e6301dfc36fb91b18dbc5252
Henrik Johansson <henrik.johansson@redpill-linpro.com>
2013-08-29 09:02:27 +0200

    More on interfaces

--- examples/interfaces.go
+++ examples/interfaces.go
@@ -10,6 +10,24 @@ type Item int8
 func (i *Item) Create() {}
 
 func main() {
-	// Force checking that our iten actually implements Useable
+	// Force checking that our item actually implements Useable
 	var _ Useable = new(Item)
 }
+
+// EMPTYIFSTART OMIT
+func ComplexGenericStuff(i interface{}) string {
+	switch str := i.(type) {
+	case string:
+		return str
+	case Stringer:
+		return str.String()
+	}
+
+	panic("Only strings or string like objects supported!")
+}
+
+type Stringer interface {
+	String() string
+}
+
+// EMPTYIFSTOP OMIT
